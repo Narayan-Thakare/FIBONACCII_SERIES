5027b82eabdd1569733b44dd7c72d6cdde93fa45
Narayan Thakare <narayan111thakare@gmail.com>
2024-07-21 21:36:01 +0530

    first commit

---
+++ src/Fibonacci.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	Scanner s = new Scanner(System.in);
+		
+		int a = 0;
+	 	int b = 1,c;
+		
+	 	System.out.println("enter the no. want to do in that range ");
+	 	
+	 	int range  =s.nextInt();
+	 	
+	 	for (int i = 1 ;i <=range ;i++){
+	 		
+	 		
+	 		c =a+b;
+	 		a=b;
+	 		b=c;
+	 		System.out.println(c);
+	 		
+	 		
+	 	}
+		
+		
+		
+	}
+
+}
